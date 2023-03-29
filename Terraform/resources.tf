b13442c6df4dafa31f7bdee85e44b1e263ce75b2
Augusto Cezar Ribeiro Santana <cezaodabahia@gmail.com>
2023-03-29 18:17:55 -0300

    Terraform

--- Terraform/resources.tf
+++ Terraform/resources.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "EC2_Instance" {
-  instance_type = "t2.micro"
-  ami           = "ami-007855ac798b5175e"
+  instance_type = var.instanceType
+  ami           = var.imageId
   tags = {
     Name = "cezaodabahia.CriadaPeloTerraform"
   }
